fcf2dc4b488656656a6b9b045f2c87dc1b1940ec
yan.hemba <yan.hemba@toweya.net>
2019-07-29 10:08:45 +0300

    API KEY

--- tets.py
+++ tets.py
@@ -2,16 +2,22 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import requests
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/historical'
 parameters = {
-  'start':'1',
-  'limit':'5000',
-  'convert':'USD'
+  'start': '1',
+  'limit': '1',
+  'convert': 'USD,BTC',
+  'date': '2019-01-29',
+
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+  'X-CMC_PRO_API_KEY': '9d0051fa-6435-44ab-8e28-15be0a598018',
 }
+
+investments = input("Input your money ")
+
+month = input("Input your month ")
 
 session = Session()
 session.headers.update(headers)
